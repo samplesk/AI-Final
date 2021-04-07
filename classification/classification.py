9bf3e12f7721e2b728a661c0e4f24f2512ab6e26
samplesk <56133274+samplesk@users.noreply.github.com>
2021-04-07 14:54:34 -0500

    4/7

--- classification/classification.py
+++ classification/classification.py
@@ -1,7 +1,5 @@
 #learning from 18 attributes (animal name, 15 Boolean, 2 numerics)
 #The final number is the set it falls into, 1 through 7
-
-zoo.dataloader = torch.utils.DataLoader(dataset, batch_size=1, suffle=False)
  
 #loop for lines in set
 #loop for each attribute
@@ -15,3 +13,8 @@ zoo.dataloader = torch.utils.DataLoader(dataset, batch_size=1, suffle=False)
     #is aquatic
     #.....
 #compile and decide which of 7 categories it goes in
+
+#pip install -U scikit-learn
+#pip install numpy
+#python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+
