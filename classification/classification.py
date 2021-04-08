51817a6f5b328d931e113d97073681156871530a
samplesk <56133274+samplesk@users.noreply.github.com>
2021-04-08 09:46:13 -0500

    4/8

--- classification/classification.py
+++ classification/classification.py
@@ -1,17 +1,17 @@
-import numpy as pydataPath 
-import numpy as np
-import pandas as pd 
+#import numpy as pydataPath 
+#import numpy as np
 #from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
-from sklearn import datasets
+#from sklearn import preprocessing
+#from sklearn import datasets
 #from sklearn.ensemble import RandomForestRegressor
-from sklearn.pipeline import make_pipeline
+#from sklearn.pipeline import make_pipeline
 #from sklearn.model_selection import GridSearchCV
 #from sklearn.metrics import mean_squared_error, r2_score
-from sklearn import tree
-from sklearn.tree import DecisionTreeClassifier
+#from sklearn import tree
+#from sklearn.tree import DecisionTreeClassifier
 
+import pandas as pd 
 
 import csv
-digits = pd.read_csv("data.csv", header=None)
+digits = pd.read_csv("classification/zoo.data", header=None)
 print(digits)
