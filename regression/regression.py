70aced3ed7994c185dad148673327f5caeade700
VictoriaFarr <54329700+VictoriaFarr@users.noreply.github.com>
2021-04-22 19:08:33 -0500

    r
    r

--- regression/regression.py
+++ regression/regression.py
@@ -1,31 +1,26 @@
 import pandas as pd
 import numpy as np
-from sklearn import datasets
+#from sklearn import datasets
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 
 # Path of the file to read
-myData = pd.read_csv(r'C:\Users\victo\Downloads\RealEstDataSet.csv', header=None) 
-#, usecols=[4,7]
+myData = pd.read_csv(r'C:\Users\victo\Downloads\RealEstDataSet.csv', usecols=[3,6], header=1) 
 
-x = myData.data
-y = myData.target
 
-print("x")
-print(x)
-print(x.shape)
-print("y")
-print(y)
-print(y.shape)
+#x = myData[3]
+#y = myData[6]
+
+
 #pull in more datasets and label them to use
-#X = data.iloc[:,:-1].values  #independent variable array
-#y = data.iloc[:,1].values  #dependent variable vector
+X = myData.iloc[:,:-1].values  #independent variable array
+y = myData.iloc[:,1].values  #dependent variable vector
 
 #split the data set into 1/3 for test and 2/3 for training
 # from sklearn.model_selection import train_test_split
-# X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=1/3,random_state=0) 
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=1/3,random_state=0) 
 
 #from sklearn.linear_model import LinearRegression
-#regressor = LinearRegression()
-#regressor.fit(X_train,y_train)
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
