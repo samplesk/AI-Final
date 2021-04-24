b33172a97cd170508735ee47d92494421112ebb9
VictoriaFarr <54329700+VictoriaFarr@users.noreply.github.com>
2021-04-23 20:16:31 -0500

    Update regression.py

--- regression/regression.py
+++ regression/regression.py
@@ -2,25 +2,45 @@ import pandas as pd
 import numpy as np
 #from sklearn import datasets
 from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 
 # Path of the file to read
-myData = pd.read_csv(r'C:\Users\victo\Downloads\RealEstDataSet.csv', usecols=[3,6], header=1) 
-
-
-#x = myData[3]
-#y = myData[6]
-
-
+myData = pd.read_csv(r'C:\Users\victo\Downloads\RealEstDataSet.csv', header=1)
+myData = myData.rename(columns=['number','X1','houseAge','X3','X4','X5','X6','housePrice'], inplace = False)
+#print(myData.shape)
+#myData.shape  #414, 8
+#myData.describe
+#myData.plot(X='X2 house age', y='Y house price of unit area', style='o')
+#print(myData.feature_names)
 #pull in more datasets and label them to use
-X = myData.iloc[:,:-1].values  #independent variable array
-y = myData.iloc[:,1].values  #dependent variable vector
-
+X = myData[['houseAge']]
+#X = myData[3]
+y = myData[['housePrice']]
+#print(X)
+#print(y)
+#X = myData.iloc[:,:-1].values  #independent variable array
+#y = myData.iloc[:,8].values  #dependent variable vector
+#print(y)
 #split the data set into 1/3 for test and 2/3 for training
 # from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=1/3,random_state=0) 
+
+X_train, X_test, y_train, y_test = train_test_split(X,y.target,test_size=.2, train_size=1) 
 
 #from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-regressor.fit(X_train,y_train)
+regressor.fit(X_train, y_train)
+model.score(X_test, y_test)
+
+#regressor.fit(X_train,y_train)
+
+#plt.scatter(X.T[3], y_train)
+#plt.show
+#plt.scatter(X_train, y_train,  color='black')
+#plt.plot(X_train, y_train, color='blue', linewidth=3)
+
+#plt.xticks(())
+#plt.yticks(())
+
+#plt.show()
