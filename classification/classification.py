38cdfb577b68e17f92d32b6a1b0372d18e06154e
samplesk <56133274+samplesk@users.noreply.github.com>
2021-04-27 14:54:10 -0500

    Update classification.py

--- classification/classification.py
+++ classification/classification.py
@@ -99,7 +99,7 @@ realEstate = pd.read_csv("realEstate.csv")
 #---------------------realEstate
 from sklearn.neighbors import KNeighborsRegressor
 
-X = realEstate[['X2 house age','X3 distance to the nearest MRT station','X5 latitude','X6 longitude']]
+X = realEstate[['X2 house age','X3 distance to the nearest MRT station','X4 number of convenience stores','X5 latitude','X6 longitude']]
 y = realEstate[['Y house price of unit area']]
 
 knn = KNeighborsRegressor(n_neighbors = 25, weights = 'distance')
